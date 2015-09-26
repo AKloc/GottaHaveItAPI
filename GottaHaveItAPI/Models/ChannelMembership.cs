0b2f5abb39f6b94b406ced5ac287d62446657cce
Andy Kloc <andy.kloc@gmail.com>
2015-09-26 16:27:09 -0400

    Added ChannelMembership.

---
+++ GottaHaveItAPI/Models/ChannelMembership.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GottaHaveItAPI.Models
+{
+    class ChannelMembership
+    {
+        public int ID { get; set; }
+        public int ChannelID { get; set; }
+        public int EventID { get; set; }
+        public bool IsActive { get; set; }
+
+
+        public virtual Event Event { get; set; }
+        public virtual Channel Channel { get; set; }
+    }
+}
